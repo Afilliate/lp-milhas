dc987ff25ab2a0f6dfbcb2edc96048891ae10f79
VPN <140664066+Afilliate@users.noreply.github.com>
2026-07-29 21:21:50 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'esbuild', // usa o minificador nativo do Vite
-    // Remova a seção 'terserOptions' inteira
+    minify: 'esbuild', // minificador nativo, sem necessidade de instalar nada
   }
 })
